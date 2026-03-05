857ed53b2f6ab2ff8949705d1c3fe70a23439d84
jimmyvargaslozanojr <zirymmij@gmail.com>
2026-03-05 16:56:02 +0800

    fix authentication using firebase

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+window.AUTH_CONFIG = {
+  FIREBASE_API_KEY: "AIzaSyDjhv-uj81Am43MXwRquinlRRoxBE9NGQA",
+  FIREBASE_AUTH_DOMAIN: "gettingstartedwithfireba-efd5b.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "gettingstartedwithfireba-efd5b",
+  FIREBASE_APP_ID: "1:617609354798:web:5f804b34cad0c5f388c9bf",
+  FIREBASE_ROLES_CLAIM: "roles",
+  FIREBASE_ALLOWED_LOGIN_ROLES: "",
+  FIREBASE_ADMIN_EMAILS: "",
+  FIREBASE_ANALYST_DOMAIN: "@nia.gov.ph"
+};
